d3f4f9741cca4e1374babdb225f938ea5fb424e5
Gabriel Rizk <gabrielrizk1@gmail.com>
2021-03-24 15:42:42 -0400

    Added func to get timestamp and latency time

--- catjam/public/send_timestamp.js
+++ catjam/public/send_timestamp.js
@@ -5,6 +5,7 @@ async function sendTimeToServer() {
     // This is the port where we will run our web server
     const port = 4000;
     console.log('sendTimeToServer called');
+
     const response = await fetch('http://localhost:' + port + '/ping', {
         method: 'POST',
         headers: {
@@ -12,6 +13,8 @@ async function sendTimeToServer() {
         },
         body: JSON.stringify({
                 time: time
+                time_diff : get_time_diff()
+                client_id : get_user_id()
             })
         })
         .then(data => {
@@ -21,4 +24,18 @@ async function sendTimeToServer() {
             console.error('Error:', error);
         });
     console.log('Received response ' + response + ' from server');
+}
+
+function get_user_id() {
+  window.Twitch.ext.onAuthorized(function(auth) {
+  console.log('The channel ID is', auth.channelId);
+  return auth.userId
+});
+}
+
+function get_time_diff() {
+   window.Twitch.ext.onContext(function(contextCallback) {
+     return contextCallback.context.hlsLatencyBroadcaster
+  });
+
 }
